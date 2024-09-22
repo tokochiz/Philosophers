1cad4eb08ab20685ceae57d1c5b1a2690527e409
tokochiz <tokohana.1003@gmail.com>
2024-09-22 22:00:07 +0900

    fix : 概要の実装のみ

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: ctokoyod <ctokoyod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/22 16:52:47 by ctokoyod          #+#    #+#             */
-/*   Updated: 2024/09/22 19:24:41 by ctokoyod         ###   ########.fr       */
+/*   Updated: 2024/09/22 21:58:57 by ctokoyod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,7 @@
 データ構造の設計
 初期化関数の実装
 　・コマンドライン引数をパースする関数
-　・哲学者とフォークの初期化状態を設定する
-　・ミューテックスを初期化する関数
+
 哲学者の行動を実装
 メインループの実装
 クリーンアップ関数の実装
@@ -28,33 +27,113 @@
 テスト検証
 
 */
-
-int				cnt = 0;
-pthread_mutex_t	mutex;
-
-void	*routine(void *p)
+// todo : 引数をパースする関数
+int	parse_arguments(int argc, char **argv, t_data *data)
 {
-	int	i;
-    (void)p;
-	i = 0;
-	while (i < 10000)
-	{
-		//pthread_mutex_lock(&mutex);
-		cnt++;
-		//pthread_mutex_unlock(&mutex);
-		i++;
-	}
-	return (NULL);
+	if (argc != 5 && argc != 6)
+		return 1;
+	data->number_of_philosophers = ft_atoi(argv[1]);
+	data->time_to_die = ft_atoi(argv[2]);
+	data->time_to_die = ft_atoi(argv[3]);
+	data->time_to_sleep = ft_atoi(argv[4]);
+	if (argc == 6)
+		data->num_must_eat = ft_atoi(argv[5]);
+	else
+		data->num_must_eat = -1;
+	if (data->number_of_philosophers <= 0 || data->time_to_die <= 0
+		|| data->time_to_eat <= 0 || data->time_to_sleep <= 0 || (argc == 6
+			&& data->num_must_eat <= 0))
+		return (1);
+	return (0);
 }
 
-int	main(void)
+// todo : 哲学者とフォークの初期状態を設定する
+int	init_forks(t_data *data)
 {
-	pthread_t p1, p2;
-	pthread_mutex_init(&mutex, NULL);
-	pthread_create(&p1, NULL, &routine, NULL);
-	pthread_create(&p2, NULL, &routine, NULL);
-	pthread_join(p1, NULL);
-	pthread_join(p2, NULL);
-	pthread_mutex_destroy(&mutex);
-	printf("cnt -> %d\n", cnt);
+	int	i;
+
+	data->forks = (t_fork *)malloc(sizeof(t_fork)
+			* data->number_of_philosophers);
+	if (data->forks == NULL)
+		return (1);
+	i = 0;
+	while (i < data->number_of_philosophers)
+	{
+		data->philos[i].id = i + 1;
+		data->philos[i].state = THINKING;
+		data->philos[i].eat_count = 0;
+		data->philos[i].last_meal_time = 0;
+		data->philos[i].data = data;
+		data->forks[i].id = i + 1;
+		i++;
+	}
+	return (0);
+}
+
+int	init_philos(t_data *data)
+{
+	int	i;
+
+	data->philos = (t_philo *)malloc(sizeof(t_philo)
+			* data->number_of_philosophers);
+	if (data->philos == NULL)
+		return (1);
+	i = 0;
+	while (i < data->number_of_philosophers)
+	{
+		data->philos[i].id = i + 1;
+		data->philos[i].state = THINKING;
+		data->philos[i].eat_count = 0;
+		data->philos[i].last_meal_time = 0;
+		data->philos[i].data = data;
+		data->forks[i].id = i + 1;
+		i++;
+	}
+	return (0);
+}
+
+// todo : ミューテックスを初期化する関数
+int	init_mutexes(t_data *data)
+{
+	int	i;
+
+	if (pthread_mutex_init(&data->death_mutex, NULL) != 0)
+		return (1);
+	if (pthread_mutex_init(&data->print_mutex, NULL) != 0)
+		return (1);
+	i = 0;
+	while (i < data->num_must_eat)
+	{
+		if (pthread_mutex_init(&data->forks[i].mutex, NULL) != 0)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	t_data	data;
+
+	// 引数のパース、初期化
+	if (parse_arguments(argc, argv, &data))
+		print_error();
+	if (init_philos(&data))
+		print_error();
+	if (init_forks(&data))
+		print_error();
+	if (init_mutexes(&data))
+		print_error();
+		
+	// todo : 哲学者のスレッドを作成、哲学者の動作を始める
+	// 失敗したらすべてのスレッドをミューテックスを開放しないといけない
+	// if (start_eating(&data))
+	// {
+	// 	release_data(&data);
+	// 	print_error();
+	// }
+	// // todo : 哲学者のモニタリング　死ぬか、シミュレートが終了するまでの間監視する
+	// monitor_philos(&data);
+	// // todo : 哲学者が死んだ場合、食べることに失敗した場合
+	// death_data(&data);
 }
