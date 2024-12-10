a2bb69b9aa5e403eaa21b386d2c9c894376f0f29
tokochiz <tokohana.1003@gmail.com>
2024-12-10 21:15:04 +0900

    delete : rewriteする

--- srcs/main.c
+++ srcs/main.c
@@ -5,85 +5,24 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ctokoyod <ctokoyod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/22 16:52:47 by ctokoyod          #+#    #+#             */
-/*   Updated: 2024/11/30 19:55:53 by ctokoyod         ###   ########.fr       */
+/*   Created: 2024/12/10 20:39:45 by ctokoyod          #+#    #+#             */
+/*   Updated: 2024/12/10 21:04:57 by ctokoyod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/philo.h"
-
-/* todo
-プロジェクトの構造設計
-データ構造の設計
-初期化関数の実装
-　・コマンドライン引数をパースする関数
-
-哲学者の行動を実装
-メインループの実装
-クリーンアップ関数の実装
-デットロック回避とスターベーション防止
-エラー処理
-最適化
-テスト検証
-
-*/
-
-// todo : 引数をパースする関数
-int	parse_arguments(int argc, char **argv, t_data *data)
-{
-	if (argc != 5 && argc != 6)
-	{
-		 fprintf(stderr, "Usage: %s number_of_philosophers time_to_die time_to_eat time_to_sleep [must_eat_times]\n", argv[0]);
-		return (1);
-	}
-	data->number_of_philosophers = ft_atoi(argv[1]);
-	data->time_to_die = ft_atoi(argv[2]);
-	data->time_to_eat = ft_atoi(argv[3]);
-	data->time_to_sleep = ft_atoi(argv[4]);
-	// printf("***argv debag %d %d %d %d\n", data->number_of_philosophers,
-	// 	data->time_to_die, data->time_to_eat, data->time_to_sleep);
-	if (argc == 6)
-		data->must_eat_times = ft_atoi(argv[5]);
-	else
-		data->must_eat_times = -1;
-	if (data->number_of_philosophers <= 0 || 
-	data->time_to_die <= 0 || 
-	data->time_to_eat <= 0 || 
-	data->time_to_sleep <= 0 || 
-	(argc == 6 && data->must_eat_times <= 0)){
-	 fprintf(stderr, "Error: All parameters must be positive integers\n");
-		return (1);
-	}
-	return (0);
-}
+#include "philo.h"
 
 int	main(int argc, char **argv)
 {
-	t_data	data;
+	t_table	data;
 
-	if (parse_arguments(argc, argv, &data))
-		print_error();
-	init_data(&data);
-	// 引数のパース、初期化
-
-    printf("=== Starting test with parameters ===\n");
-    printf("Philosophers: %d\n", data.number_of_philosophers);
-    printf("Time to die: %lld ms\n", data.time_to_die);
-    printf("Time to eat: %lld ms\n", data.time_to_eat);
-    printf("Time to sleep: %lld ms\n", data.time_to_sleep);
-    if (argc == 6)
-        printf("Must eat count: %lld\n", data.must_eat_times);
-    printf("================================\n");
-
-		// todo : 哲学者のスレッドを作成、哲学者の動作を始める
-		// 失敗したらすべてのスレッドをミューテックスを開放しないといけない
-	if (start_philos(&data))
-	{
-		// release_data(&data);
-		//print_error();
-	}
-	// todo : 哲学者が死んだ場合、食べることに失敗した場合
-	//death_data(&data);
+	if (!check_arg(argc, argv))
+		return (printf("Error\n"));
+	if (!init_data(argc, argv, &data))
+		return (printf("Error\n"));
+	start_table(&data);
+	if (data.num_of_philos != 1)
+		join_threads(&data); //全てのスレッドの終了を待機
+        //例外的に、哲学者が1人の場合は特別な処理
 	return (0);
 }
-
