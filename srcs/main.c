839c5cf303910fc225a9dc62bdb82df389880136
tokochiz <tokohana.1003@gmail.com>
2024-12-14 21:18:17 +0900

    add : すべてのスレッドの終了を待機するため、join_all_pthreasを追加

--- srcs/main.c
+++ srcs/main.c
@@ -6,24 +6,37 @@
 /*   By: ctokoyod <ctokoyod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 20:39:45 by ctokoyod          #+#    #+#             */
-/*   Updated: 2024/12/11 20:03:59 by ctokoyod         ###   ########.fr       */
+/*   Updated: 2024/12/14 20:18:33 by ctokoyod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philo.h"
+#include "../include/philo.h"
 
 // TODO : 全てのスレッドの終了を待機する関数作成　例外的に、哲学者が1人の場合は特別な処理
+void	join_all_threads(t_table *table)
+{
+	int	i;
+
+	i = 0;
+	while (i < table->num_of_philos)
+	{
+		pthread_join(table->philos[i].thread_id, NULL);
+		i++;
+	}
+	pthread_join(table->monitor, NULL);
+}
 
 int	main(int argc, char **argv)
 {
 	t_table	table;
 
 	if (!check_arg(argc, argv))
-		return (printf("Error\n"));
+		return (printf("Error1\n"));
 	if (!init_data(argc, argv, &table))
-		return (printf("Error\n"));
+		return (printf("Error2\n"));
 	start_table(&table);
 	if (table.num_of_philos != 1)
-		join_threads(&table);
+		join_all_threads(&table);
+	end_table(&table);
 	return (0);
 }
