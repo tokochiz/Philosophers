d71decb2a0e4e8c85fd7138c07d1168476d1ccc3
tokochiz <tokohana.1003@gmail.com>
2024-12-16 21:39:57 +0900

    add : 全哲学者の健康状態を監視する機能

--- srcs/context/monitor.c
+++ srcs/context/monitor.c
@@ -6,9 +6,101 @@
 /*   By: ctokoyod <ctokoyod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/14 21:00:52 by ctokoyod          #+#    #+#             */
-/*   Updated: 2024/12/14 21:11:39 by ctokoyod         ###   ########.fr       */
+/*   Updated: 2024/12/16 21:36:28 by ctokoyod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/philo.h"
 
+// TODO : 哲学者死んだ、
+
+// TODO : モニターのメイン関数
+// TODO : `time_to_die`  死ぬまでの時間 (ms)を超えたか
+// TODO : `[optional: num_of_must_eat]` プログラムを終了する食事回数を超えたか
+// すべての哲学者が終了条件を満たしていたら、プログラムを終了する　これを繰り返し行う
+
+// 死亡したときの処理
+static void	_handle_philo_death(t_philo *philo)
+{
+	pthread_mutex_unlock(&philo->lock);            // 哲学者のロックを解放
+	pthread_mutex_lock(&philo->table->table_lock); //テーブルのロックを取得
+	philo->table->end_flag = 1;
+	pthread_mutex_unlock(&philo->table->table_lock);
+}
+
+// philoの健康状態をチェック
+static bool	_check_philo_health(t_philo *philo)
+{
+	long	current_time;
+
+	current_time = get_current_time_ms();
+	pthread_mutex_lock(&philo->lock);
+	// 少なくとも1度は食事したか、食事から生存限界を超えているか、食事中ではないか
+	if (philo->last_meal_time != -1 && current_time
+		- philo->last_meal_time >= philo->time_to_die && philo->is_eating == 0)
+	{
+		_handle_philo_death(philo);
+		return (true);
+	}
+	pthread_mutex_unlock(&philo->lock);
+	return (false);
+}
+
+static bool	_check_philo_must_eat(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->lock);
+	if (!(philo->is_full) && philo->table->num_of_must_eat > 0
+		&& philo->eat_count >= philo->table->num_of_finish)
+	{
+		philo->is_full = true;
+		pthread_mutex_unlock(&philo->lock);
+		pthread_mutex_lock(&philo->table->table_lock);
+		philo->table->num_of_finish++;
+		if (philo->table->num_of_finish >= philo->table->num_of_philos)
+		{
+			philo->table->end_flag = 1;
+			pthread_mutex_unlock(&philo->table->table_lock);
+			return (true);
+		}
+		pthread_mutex_unlock(&philo->table->table_lock);
+	}
+	else
+	{
+		pthread_mutex_unlock(&philo->lock);
+	}
+	return (false);
+}
+
+static bool	_can_stop_monitoring(t_philo *philo)
+{
+	if (_check_philo_health(philo))
+		return (true);
+	if (_check_philo_must_eat(philo))
+		return (true);
+	return (false);
+}
+
+void	*monitor_all_philos(void *arg)
+{
+	t_table	*table;
+	t_philo	*philo;
+	int		i;
+
+	table = (t_table *)arg;
+	i = 0;
+	get_sleep_time_ms(10);
+	while (1)
+	{
+		philo = &table->philos[i];
+		if (_can_stop_monitoring(philo))
+		{
+			return (NULL);
+		}
+		i++;
+		if (i == table->num_of_philos)
+		{
+			i = 0;
+		}
+	}
+	return (NULL);
+}
