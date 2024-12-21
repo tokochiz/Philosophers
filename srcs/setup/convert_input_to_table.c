73151418eacec717ddfb27fc4abb1fa1eb93a3a0
tokochiz <tokohana.1003@gmail.com>
2024-12-21 16:06:29 +0900

    fix : 関数名の変更

---
+++ srcs/setup/convert_input_to_table.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   convert_input_to_table.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ctokoyod <ctokoyod@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/11 20:51:57 by ctokoyod          #+#    #+#             */
+/*   Updated: 2024/12/21 16:02:33 by ctokoyod         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/philo.h"
+
+bool	validate_input_parameters(t_table *table)
+{
+	if (table->num_of_philos == -1 || table->time_to_die == -1
+		|| table->time_to_eat == -1 || table->time_to_sleep == -1)
+		return (false);
+	else if (table->num_of_philos == 0 || table->time_to_die == 0
+		|| table->time_to_eat == 0 || table->time_to_sleep == 0)
+		return (false);
+	else if (table->num_of_must_eat > 200)
+		return (false);
+	return (true);
+}
+
+void	convert_input_to_table(int argc, char **argv, t_table *table)
+{
+	table->num_of_philos = ft_atoi(argv[1]);
+	table->time_to_die = ft_atoi(argv[2]);
+	table->time_to_eat = ft_atoi(argv[3]);
+	table->time_to_sleep = ft_atoi(argv[4]);
+	table->end_flag = false;
+	table->num_of_finish = 0;
+	if (argc == 6)
+		table->num_of_must_eat = ft_atoi(argv[5]);
+	else
+		table->num_of_must_eat = -1;
+}
