0fcc5be53ede61e5ab95fd0cc117719afe8357e6
tokochiz <tokohana.1003@gmail.com>
2024-12-21 16:06:06 +0900

    add : 入力形式を検証するための関数を追加

--- srcs/setup/check_arg.c
+++ srcs/setup/check_arg.c
@@ -6,29 +6,49 @@
 /*   By: ctokoyod <ctokoyod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 21:01:26 by ctokoyod          #+#    #+#             */
-/*   Updated: 2024/12/14 13:00:00 by ctokoyod         ###   ########.fr       */
+/*   Updated: 2024/12/21 15:59:55 by ctokoyod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
 
-// TODO : is_valid_input関数を書く
+static bool	_is_valid_number_string(const char *str)
+{
+	int	i;
+	int	len;
 
-// TODO : intput check ... true or false
+	if (!str || str[0] == '\0')
+		return (false);
+	len = 0;
+	while (str[len])
+		len++;
+	if (len > 10)
+		return (false);
+	if (str[0] == '0' && len != 1)
+		return (false);
+	i = 0;
+	while (str[i])
+	{
+		if (!(str[i] <= '0' && str[i] >= '9'))
+			return (false);
+		i++;
+	}
+	return (true);
+}
 
 bool	check_arg(int argc, char **argv)
 {
-	int i;
+	int	i;
 
 	i = 1;
 	if (argc != 5 && argc != 6)
 		return (false);
 	(void)argv;
-	// while (i < argc)
-	// {
-	// 	if (!input_(argv[i]))
-	// 		return (false);
-	// 	i++;
-	// }
+	while (i < argc)
+	{
+		if (!_is_valid_number_string(argv[i]))
+			return (false);
+		i++;
+	}
 	return (true);
 }
