2e35cd93ec282e063817b44bd14f05ff935626a1
tokochiz <tokohana.1003@gmail.com>
2025-01-06 18:44:18 +0900

    fix : デッドロック回避のため循環待機をしない

--- philo/srcs/context/fork.c
+++ philo/srcs/context/fork.c
@@ -6,7 +6,7 @@
 /*   By: ctokoyod <ctokoyod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/14 18:44:11 by ctokoyod          #+#    #+#             */
-/*   Updated: 2025/01/02 14:26:54 by ctokoyod         ###   ########.fr       */
+/*   Updated: 2025/01/06 18:43:06 by ctokoyod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,25 @@
 
 int	take_fork(t_philo *philo)
 {
-	if (pthread_mutex_lock(philo->left_fork) != 0)
-		return (-1);
-	if (pthread_mutex_lock(philo->right_fork) != 0)
+	if (philo->id == philo->table->num_of_philos)
 	{
-		pthread_mutex_unlock(philo->left_fork);
-		return (-1);
+		if (pthread_mutex_lock(philo->right_fork) != 0)
+			return (-1);
+		if (pthread_mutex_lock(philo->left_fork) != 0)
+		{
+			pthread_mutex_unlock(philo->right_fork);
+			return (-1);
+		}
+	}
+	else
+	{
+		if (pthread_mutex_lock(philo->left_fork) != 0)
+			return (-1);
+		if (pthread_mutex_lock(philo->right_fork) != 0)
+		{
+			pthread_mutex_unlock(philo->left_fork);
+			return (-1);
+		}
 	}
 	return (0);
 }
